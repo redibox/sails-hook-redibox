24cd72e8e63675a110ee4bac746db6d816b8e1c6
Salakar <mike.diarmid@gmail.com>
2016-04-14 13:24:04 +0100

    linting and dependency updates

--- src/index.js
+++ src/index.js
@@ -29,27 +29,21 @@ import RediBox from 'redibox';
 export default function (sails) {
   const services = require('sails-util-mvcsloader')(sails);
   return {
-    initialize: function (next) {
+    initialize(next) {
       if (!sails.config.redibox && !sails.config.hooks.redibox) {
-        return next(new Error('No RediBox config found at sails.config.hooks.redibox, aborting!'));
-      }
-      const eventsToWaitFor = ['hook:orm:loaded'];
-
-      sails.after(eventsToWaitFor, () => {
-        this.instance = new RediBox(sails.config.redibox || sails.config.hooks.redibox);
-        this.instance.on('ready', function (status) {
-          sails.log.verbose('Redibox: ', status);
-          services.injectAll({
-            controllers: __dirname + '/api/controllers', // Path to your hook's controllers
-            services: __dirname + '/api/services' // Path to your hook's services
-          }, function (error) {
-            return next(error);
+        next(new Error('No RediBox config found at sails.config.hooks.redibox, aborting!'));
+      } else {
+        sails.after(['hook:orm:loaded'], () => {
+          this.instance = new RediBox(sails.config.redibox || sails.config.hooks.redibox);
+          this.instance.on('ready', (status) => {
+            sails.log.verbose('Redibox: ', status);
+            services.injectAll({
+              controllers: `${__dirname}/api/controllers`, // Path to your hook's controllers
+              services: `${__dirname}/api/services`, // Path to your hook's services
+            }, error => next(error));
           });
         });
-      });
-    }
+      }
+    },
   };
 }
-
-
-
