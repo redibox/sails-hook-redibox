bd50f1b65a39911c34dd6759887ef42148c73051
Salakar <mike.diarmid@gmail.com>
2016-07-27 16:41:23 +0100

    globals for hooks are now auto created from hooks object keys

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 import RediBox from 'redibox';
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export default function (sails) {
-  const services = require('sails-util-mvcsloader')(sails);
   return {
     initialize(next) {
       if (!sails.config.redibox && !sails.config.hooks.redibox) {
-        next(new Error('No RediBox config found at sails.config.hooks.redibox, aborting!'));
-      } else {
-        sails.after(['hook:orm:loaded'], () => {
-          this.instance = new RediBox(sails.config.redibox || sails.config.hooks.redibox);
-          this.instance.on('ready', (status) => {
-            sails.log.verbose('Redibox: ', status);
-            services.injectAll({
-              controllers: `${__dirname}/api/controllers`, // Path to your hook's controllers
-              services: `${__dirname}/api/services`, // Path to your hook's services
-            }, error => next(error));
-          });
-        });
+        sails.log.warn('No RediBox config found at sails.config.redibox, aborting redibox load!');
+        return next();
       }
+
+      return sails.after(['hook:orm:loaded'], () => {
+        this.instance = new RediBox(sails.config.redibox || sails.config.hooks.redibox);
+        this.instance.on('ready', (status) => {
+          const hooks = Object.keys(this.instance.hooks);
+          sails.log.verbose(!status ? 'Redibox is now ready!' : status);
+          global.RediBox = this.instance;
+          for (let i = 0, iLen = hooks.length; i < iLen; i++) {
+            const key = hooks[i];
+            global[capitalizeFirstLetter(key)] = this.instance.hooks[key];
+          }
+          next();
+        });
+      });
     },
   };
 }
